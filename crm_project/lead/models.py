800c1b14e58818f098c84da17b78cb678af830e2
Emmanuel <97593713+Godson-Ozioma@users.noreply.github.com>
2025-08-20 12:53:30 +0100

    Changes to Leads

--- crm_project/lead/models.py
+++ crm_project/lead/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Lead(models.Model):
-    created_by = models.ForeignKey()
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+    
+    
+    created_by = models.ForeignKey(User, related_name='leads', on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
